280a40968085f9f0654a14de58c3ee6b94127e19
Karlamax178 <113449348+Karlamax178@users.noreply.github.com>
2026-09-08 20:56:19 -0600

    Corrección
    De igual manera acá después de correrlo en python, me tocó hacerle un par de correcciones para que cuadrara con las funciones que definí anteriormente

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 #Probando mi código
-from funciones import validar_matrices, ocupacion_sobrecarga
+from funciones import matrices, ocupacion_sobrecarga
 
 cargas = [
-    [100.0, 230.0, 200.0],
-    [75.0,  700.0, 120.0]
+    [100.0, 250.0, 200.0],
+    [20.0,  300.0, 125.0]
 ]
 
 capacidades = [
-    [100.0, 230.0, 200.0],
-    [75.0,  700.0, 120.0]
+    [200.0, 200.0, 200.0],
+    [100.0, 250.0, 150.0]
 ]
 
 print ("----PRUEBA DE CARGA----")
 
-es_valida = validar_matrices(cargas, capacidades)
+es_valida = matrices(cargas, capacidades)
 print(f"1. ¿Matrices son válidas y también coherentes?: {es_valida}")
 
 if es_valida:
-  porcentajes, sobrecargas = ocupacion_sobrecargas(cargas, capacidades)
+  porcentajes, sobrecargas = ocupacion_sobrecarga(cargas, capacidades)
   print ("\n2. Matriz de Ocupación (%):")
   for fila in porcentajes:
     print (" ", fila)
